e50ab7828c269b6859e533138ec6f91253591a83
Philippe Kalitine <philippe.kalitine@gmail.com>
2016-06-01 16:19:44 +0200

    change turn server

--- netflux.js
+++ netflux.js
@@ -1648,7 +1648,7 @@
         iceServers: [
           {urls: 'stun:23.21.150.121'},
           {urls: 'stun:stun.l.google.com:19302'},
-          {urls: 'turn:numb.viagenie.ca', credential: 'webrtcdemo', username: 'louis%40mozilla.com'}
+          {urls: 'turn:192.158.29.39:3478?transport=tcp', credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=', username: '28224511:1379330808'}
         ]
       }
       this.settings = Object.assign({}, this.defaults, options)
